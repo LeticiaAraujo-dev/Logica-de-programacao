35e05de3c94b74ee18db282c4cd6c207cc72e31c
ĹeticiaAraujo-dev <leticia.p.araujo04@gmail.com>
2020-11-11 21:32:13 -0300

    teste

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Logica_de_programacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int valorUm;
+            int valorDois;
+
+            Console.WriteLine("Soma de dois números");
+            Console.WriteLine("-----------------------");
+            Console.Write("Digite o primeiro valor")
+            valorUm = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo valor")
+            valorDois = int.Parse(Console.ReadLine());
+
+            int soma = valorUm + valorDois;
+
+            Console.WriteLine("Soma de "+valorUm+" + "+valorDois+" = "+soma);
+        }
+    }
+}
